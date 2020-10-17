46aa162c445c524bce03bd22c92ca0d164054769
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-16 23:34:50 -0700

    Update SendTask.php

--- src/MilkAndCookiz/JoinTitle/SendTask.php
+++ src/MilkAndCookiz/JoinTitle/SendTask.php
@@ -15,20 +15,28 @@
 namespace MilkAndCookiz\JoinTitle;
 
 #External
-use pocketmine\{Player, Server};
+use pocketmine\Player;
+use pocketmine\Server;
 use pocketmine\scheduler\Task;
 use pocketmine\event\Listener;
 #Internal
 use MilkAndCookiz\JoinTitle\Main;
 
-class SendTask extends Task implements Listener{
+class SendTask extends Task implements Listener {
 
-	public function __construct(Main $main, Player $player){
+	public function __construct(Main $main, Player $player) {
 		$this->main = $main;
 		$this->player = $player;
 	}
 
-	public function onRun($currentTick){
-		$this->player->addTitle($this->main->getConfig()->get("main_title"), $this->main->getConfig()->get("down_title"), 20, $this->main->getConfig()->get("time_title"), 40);
+	public function onRun($currentTick) {
+		$this->player->addTitle(
+			$this->main->getConfig()->get("main_title"),
+			$this->main->getConfig()->get("down_title"),
+			20,
+			$this->main->getConfig()->get("time_title"),
+			40
+		);
 	}
-} 
+
+}
