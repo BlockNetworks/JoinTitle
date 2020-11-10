a2a8c49e07b1680fd623a4850e385be4a0b82636
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-09 21:26:50 -0800

    merge fix

--- src/MilkAndCookiz/JoinTitle/SendTask.php
+++ src/MilkAndCookiz/JoinTitle/SendTask.php
@@ -15,7 +15,7 @@
 namespace MilkAndCookiz\JoinTitle;
 
 #External
-use pocketmine\player\Player;
+use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\scheduler\Task;
 use pocketmine\event\Listener;
